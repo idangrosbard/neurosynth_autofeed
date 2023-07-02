2d768c2343c224d8a57a77f988244827ce742b11
Yuval Navon <yuvalnavon@mail.tau.ac.il>
2023-07-02 16:46:40 +0300

    After Or's comment, I've added a test file for the validator and made a few changes :)

--- validator.py
+++ validator.py
@@ -4,11 +4,14 @@ class Validator():
     """
     Class for validating the input data
     """
+
     def check_coordinates_is_int(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Checks if the coordinates are integers
-        :param list_of_coordinates: list of coordinates
-        :return: True if the coordinates are int, False otherwise
+        Args:
+            list_of_coordinates: list of coordinates
+        Returns:
+            True if the coordinates are int, False otherwise
         """
         if not all(isinstance(value, int) for coordinate in list_of_coordinates for value in coordinate):
             raise ValueError("Coordinates must be integers")
@@ -19,8 +22,10 @@ class Validator():
     def check_coordinates_length(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Checks if there are 3 coordinates for each point
-        :param list_of_coordinates: list of coordinates
-        :return: True if there are 3 coordinates for each point, False otherwise
+        Args:
+            list_of_coordinates: list of coordinates
+        Returns:
+            True if there are 3 coordinates for each point, False otherwise
         """
         if not all(len(coordinate) == 3 for coordinate in list_of_coordinates):
             raise ValueError("There must be 3 coordinates for each point")
@@ -31,8 +36,10 @@ class Validator():
     def check_coordinates_is_list(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Checks if the coordinates are in a list
-        :param list_of_coordinates: list of coordinates
-        :return: True if the coordinates are in a list, False otherwise
+        Args:
+            list_of_coordinates: list of coordinates
+        Returns:
+            True if the coordinates are in a list, False otherwise
         """
         if not isinstance(list_of_coordinates, list):
             raise ValueError("Coordinates must be in a list")
@@ -43,8 +50,10 @@ class Validator():
     def check_coordinates_is_not_empty(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Checks if the coordinates are not empty
-        :param list_of_coordinates: list of coordinates
-        :return: True if the coordinates are not empty, False otherwise
+        Args:
+            list_of_coordinates: list of coordinates
+        Returns:
+            True if the coordinates are not empty, False otherwise
         """
         if not list_of_coordinates:
             raise ValueError("Coordinates must not be empty")
@@ -59,8 +68,10 @@ class Validator():
          -90 to +90 along the x-axis
          -126 to +90 along the y-axis
          -72 to +108 along the z-axis
-        :param list_of_coordinates: list of coordinates
-        :return: True if the coordinates are in the range of the board, False otherwise
+        Args:
+            list_of_coordinates: list of coordinates
+        Returns:
+            True if the coordinates are in the range of the board, False otherwise
         """
         for coordinate in list_of_coordinates:
             if coordinate[0] < -90 or coordinate[0] > 90:
@@ -76,8 +87,10 @@ class Validator():
     def test_coordinates(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Test the coordinates
-        :param list_of_coordinates: list of coordinates
-        :return: True if the coordinates are valid, False otherwise
+        Args:
+            list_of_coordinates: list of coordinates
+        Returns:
+            True if the coordinates are valid, False otherwise
         """
         if not Validator.check_coordinates_is_list(list_of_coordinates):
             raise ValueError("Coordinates must be in a list")
@@ -93,39 +106,4 @@ class Validator():
         
         return True
     
-
-import pytest
-
-"""
-Tests for the Validator class
-"""
-
-def test_check_coordinates_is_int(validator):
-    assert validator.check_coordinates_is_int([[1, 2, 3], [4, 5, 6]])
-    assert not validator.check_coordinates_is_int([[1, 2, 3], [4, 5, 6.5]])
-
-def test_check_coordinates_length(validator):
-    assert validator.check_coordinates_length([[1, 2, 3], [4, 5, 6]])
-    assert not validator.check_coordinates_length([[1, 2, 3, 4], [4, 5, 6]])
-
-def test_check_coordinates_is_list(validator):
-    assert validator.check_coordinates_is_list([[1, 2, 3], [4, 5, 6]])
-    assert not validator.check_coordinates_is_list("1, 2, 3")
-
-def test_check_coordinates_is_not_empty(validator):
-    assert validator.check_coordinates_is_not_empty([[1, 2, 3], [4, 5, 6]])
-    assert not validator.check_coordinates_is_not_empty([])
-
-def test_check_coordinates_range(validator):
-    assert validator.check_coordinates_range([[1, 2, 3], [4, 5, 6]])
-    assert not validator.check_coordinates_range([[100, 2, 3], [4, 5, 6]])
-
-def test_test_coordinates(validator):
-    assert validator.test_coordinates([[1, 2, 3], [4, 5, 6]])
-    with pytest.raises(ValueError):
-        validator.test_coordinates([[1, 2, 3], [4, 5, 6.5]])
-        validator.test_coordinates([[1, 2, 3, 4], [4, 5, 6]])
-        validator.test_coordinates("1, 2, 3")
-        validator.test_coordinates([])
-        validator.test_coordinates([[100, 2, 3], [4, 5, 6]])
 
