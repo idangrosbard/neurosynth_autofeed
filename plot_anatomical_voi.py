f60b6209904d341d31e14ec2307f7720ca50af83
YohayZvi <Yohayzvi@gmail.com>
2023-07-02 19:39:42 +0300

    first rough draft for anatomical plotting - keep develop

--- plot_anatomical_voi.py
+++ plot_anatomical_voi.py
@@ -1,42 +1,34 @@
-import nibabel as nib
-import nilearn.plotting as plotting
+# define "plot_anatomical_voi" function which accepts 3D numpy array of VOI coordinates and plots them on anatomical image
+
+# import libraries
+import numpy as np
 import matplotlib.pyplot as plt
-from nilearn.input_data import NiftiSpheresMasker
-from nilearn.datasets import fetch_icbm152_2009
+import nibabel as nib
+from nilearn import plotting
+# import MNI template
+mni_template = nib.load('C:\\Users\\yohay\\Dropbox\\Yohay\\Studies\\MSc\\First_Year\\Semester_B\\Python_for_Neuroscience\\Assignments\\final\\MNI152_T1_2mm_brain.nii')
 
-def plot_anatomical_voi(list(voi_coords)):
+# define function
+def plot_anatomical_voi(voi_coords):
+    # create new figure
+    fig = plt.figure()
+    # plot anatomical image
+    plotting.plot_anat(mni_template, title='Anatomical image', figure=fig)
+    # plot VOI coordinates
+    plt.plot(voi_coords[:,0], voi_coords[:,1], voi_coords[:,2], 'ro')
+    # show figure
+    plt.show()
+    return fig
 
-    '''
-    Plots the VOIs on the MNI template (anatomical scan)
-    Input: x,y,z coordinates of the VOIs
-    Output: plot of the MNI brain template with the VOI
-    '''
+# test function
+# define 7 VOI coordinates as numpy array
+voi_coords = np.array([[0, 0, 0], [10, 10, 10], [20, 20, 20], [30, 30, 30], [40, 40, 40], [50, 50, 50], [60, 60, 60]])
 
-    # Load MNI template from Nilearn datasets
-    mni_template = fetch_icbm152_2009()
-    mni_img = nib.load(mni_template['t1'])
+# plot VOI coordinates on anatomical image
+plot_anatomical_voi(voi_coords)
+# show figure
+plt.show()
 
-    # Define the sphere radius for the VOI (in mm)
-    sphere_radius = 1  
 
-    masker = NiftiSpheresMasker(voi_coords, radius=sphere_radius,
-                            detrend=True, standardize=True,
-                            low_pass=None, high_pass=None, t_r=1,
-                            memory='nilearn_cache', memory_level=1, verbose=0)
 
-    # Apply the masker to create the VOI mask
-    voi_mask = masker.fit_transform(mni_img)
 
-    # Plot the MNI template
-    display = plotting.plot_anat(mni_img, title='MNI Template', draw_cross=False,
-                             display_mode='ortho', annotate=False)
-
-    # Add markers for the VOI
-    for coord in voi_coords:
-        display.add_markers(coord, marker_color='b', marker_size=100)
-
-    return plt.show()
-
-if __name__ == '__main__':
-    voi_coords = [[-45, 27, 36], [0, 30, 20], [-6, 48, -18]]
-    plot_anatomical_voi(voi_coords) 
