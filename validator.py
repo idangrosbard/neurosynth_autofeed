6f952238b03d28021deb065f8414c23e7e6256d9
Yuval Navon <yuvalnavon@mail.tau.ac.il>
2023-07-02 14:56:17 +0300

    After comments :)

--- validator.py
+++ validator.py
@@ -1,32 +1,34 @@
+from typing import List
 
-class Validator (object):
+class Validator():
     """
     Class for validating the input data
     """
-    def check_coordinates_is_int(list_of_coordinates):
+    def check_coordinates_is_int(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Checks if the coordinates are integers
         :param list_of_coordinates: list of coordinates
         :return: True if the coordinates are int, False otherwise
         """
-        for coordinate in list_of_coordinates:
-            for value in coordinate:
-                if not isinstance(value, int):
-                    raise ValueError("Coordinates must be integers")
+        if not all(isinstance(value, int) for coordinate in list_of_coordinates for value in coordinate):
+            raise ValueError("Coordinates must be integers")
+        
         return True
 
-    def check_coordinates_length(list_of_coordinates):
+
+    def check_coordinates_length(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Checks if there are 3 coordinates for each point
         :param list_of_coordinates: list of coordinates
         :return: True if there are 3 coordinates for each point, False otherwise
         """
-        for coordinate in list_of_coordinates:
-            if len(coordinate) != 3:
-                raise ValueError("There must be 3 coordinates for each point")
+        if not all(len(coordinate) == 3 for coordinate in list_of_coordinates):
+            raise ValueError("There must be 3 coordinates for each point")
+        
         return True
     
-    def check_coordinates_is_list(list_of_coordinates):
+
+    def check_coordinates_is_list(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Checks if the coordinates are in a list
         :param list_of_coordinates: list of coordinates
@@ -34,9 +36,11 @@ class Validator (object):
         """
         if not isinstance(list_of_coordinates, list):
             raise ValueError("Coordinates must be in a list")
+        
         return True
     
-    def check_coordinates_is_not_empty(list_of_coordinates):
+
+    def check_coordinates_is_not_empty(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Checks if the coordinates are not empty
         :param list_of_coordinates: list of coordinates
@@ -44,9 +48,11 @@ class Validator (object):
         """
         if not list_of_coordinates:
             raise ValueError("Coordinates must not be empty")
+        
         return True
     
-    def check_coordinates_range(list_of_coordinates):
+
+    def check_coordinates_range(self,list_of_coordinates: List[List[int]]) -> bool:
         """
         Checks if the coordinates are in the range of the board
         The range:
@@ -63,25 +69,63 @@ class Validator (object):
                 raise ValueError("The y-axis must be in the range of -126 to +90")
             if coordinate[2] < -72 or coordinate[2] > 108:
                 raise ValueError("The z-axis must be in the range of -72 to +108")
+        
         return True
     
-    def test_coordinates(list_of_coordinates):
+
+    def test_coordinates(self, list_of_coordinates: List[List[int]]) -> bool:
         """
         Test the coordinates
         :param list_of_coordinates: list of coordinates
         :return: True if the coordinates are valid, False otherwise
         """
-        if Validator.check_coordinates_is_list(list_of_coordinates) == False:
+        if not Validator.check_coordinates_is_list(list_of_coordinates):
             raise ValueError("Coordinates must be in a list")
-        if Validator.check_coordinates_is_not_empty(list_of_coordinates) == False:
+        if not Validator.check_coordinates_is_not_empty(list_of_coordinates):
             raise ValueError("Coordinates must not be empty")
-        if Validator.check_coordinates_length(list_of_coordinates) == False:
+        if not Validator.check_coordinates_length(list_of_coordinates):
             raise ValueError("There must be 3 coordinates for each point")
-        if Validator.check_coordinates_is_int(list_of_coordinates) == False:
+        if not Validator.check_coordinates_is_int(list_of_coordinates):
             raise ValueError("Coordinates must be integers")
-        if Validator.check_coordinates_range(list_of_coordinates) == False:
+        if not Validator.check_coordinates_range(list_of_coordinates):
             raise ValueError("Coordinates must be in the range of the board")
         print("Coordinates are valid - all tests passed")
+        
         return True
     
+
+import pytest
+
+"""
+Tests for the Validator class
+"""
+
+def test_check_coordinates_is_int(validator):
+    assert validator.check_coordinates_is_int([[1, 2, 3], [4, 5, 6]])
+    assert not validator.check_coordinates_is_int([[1, 2, 3], [4, 5, 6.5]])
+
+def test_check_coordinates_length(validator):
+    assert validator.check_coordinates_length([[1, 2, 3], [4, 5, 6]])
+    assert not validator.check_coordinates_length([[1, 2, 3, 4], [4, 5, 6]])
+
+def test_check_coordinates_is_list(validator):
+    assert validator.check_coordinates_is_list([[1, 2, 3], [4, 5, 6]])
+    assert not validator.check_coordinates_is_list("1, 2, 3")
+
+def test_check_coordinates_is_not_empty(validator):
+    assert validator.check_coordinates_is_not_empty([[1, 2, 3], [4, 5, 6]])
+    assert not validator.check_coordinates_is_not_empty([])
+
+def test_check_coordinates_range(validator):
+    assert validator.check_coordinates_range([[1, 2, 3], [4, 5, 6]])
+    assert not validator.check_coordinates_range([[100, 2, 3], [4, 5, 6]])
+
+def test_test_coordinates(validator):
+    assert validator.test_coordinates([[1, 2, 3], [4, 5, 6]])
+    with pytest.raises(ValueError):
+        validator.test_coordinates([[1, 2, 3], [4, 5, 6.5]])
+        validator.test_coordinates([[1, 2, 3, 4], [4, 5, 6]])
+        validator.test_coordinates("1, 2, 3")
+        validator.test_coordinates([])
+        validator.test_coordinates([[100, 2, 3], [4, 5, 6]])
 
