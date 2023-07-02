15aa4b2f156b850d1db6809d8e0a3e7b4ea1fa38
idg7 <idg7722@gmail.com>
2023-07-02 14:56:14 +0300

    Added documentation on parser functionality

--- parsers/parser.py
+++ parsers/parser.py
@@ -4,5 +4,10 @@ import pandas as pd
 class Parser(object):
     """ An interface class for parsing json answers to pandas DF rows"""
     def parse(self, json: str) -> pd.DataFrame:
-        """Parse json to pandas DF row"""
+        """Parse json to pandas DF row
+        
+        Args:
+            json (str): json to parse
+        Returns:
+            pd.DataFrame: pandas DF row"""
         raise NotImplementedError
